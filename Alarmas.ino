e4abeb28c9a9f1d72e26f76e2e668d910f491d98
AnakinSpain <antoniodavidtorres@gmail.com>
2014-10-31 20:35:13 +0100

    Alarma
    Si no hay ethernet no envia a tweeter

--- Alarmas.ino
+++ Alarmas.ino
@@ -11,15 +11,19 @@ void check_alarmas()        // Imprime el texto de las alarmas
           myGLCD.setColor(0, 255, 0);     
           myGLCD.print("SISTEMA", 50, 155);
           myGLCD.print("FUNCIONANDO", 15, 180);
-
+ 
+        if (Ethernet_Shield = true) 
+        { 
+          
         if (bitRead(msg_enviado,0) == false)
         {
           strcpy_P(buffer, (char*)pgm_read_word_near(&(tabela_textos[186])));
-        //  tweet( strcat(buffer,rtc.getTimeStr(FORMAT_LONG) ) );
+          tweet( strcat(buffer,rtc.getTimeStr(FORMAT_LONG)));
           bitWrite(msg_enviado,0,1); //Marcamos que el msg de sist ok se ha enviado
           bitWrite(msg_enviado,1,0); //y que ha sido el ultimo en enviarse
           bitWrite(msg_enviado,2,0); //asi lo dejamos preparado para enviar las alarmas
         }
+      }
         
       }
       
@@ -31,14 +35,18 @@ void check_alarmas()        // Imprime el texto de las alarmas
        myGLCD.print("TEMPERATURA", 12, 170);
        myGLCD.print("ACUARIO", 35, 185);
        
+       if (Ethernet_Shield = true) 
+       {   
+       
        if (bitRead(msg_enviado,2) == false)
        {
        strcpy_P(buffer, (char*)pgm_read_word_near(&(tabela_textos[188])));
-     //  tweet( strcat(buffer,rtc.getTimeStr(FORMAT_LONG) ) );
+       tweet( strcat(buffer,rtc.getTimeStr(FORMAT_LONG) ) );
        bitWrite(msg_enviado,2,1);//Decimos que hemos enviado el del temp agua
        bitWrite(msg_enviado,1,0);// los demas a cero, porque no se han enviado
        bitWrite(msg_enviado,0,0);
        }       
+       }
       }
       
      else if (bitRead(status_parametros,0) == true) // Si hay alarma de temperatura de tapa
